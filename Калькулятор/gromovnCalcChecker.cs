43099c527a47a2d458be1ff8f39ead5917d448f9
MikhailRudakov <90384405+MikhailRudakov@users.noreply.github.com>
2021-10-21 10:26:59 +0300

    Update gromovnCalcChecker.cs

--- Калькулятор/gromovnCalcChecker.cs
+++ Калькулятор/gromovnCalcChecker.cs
@@ -1,82 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace CalculatorMerinovLidovskoi
+
+namespace ClassLibraryPassword
 {
-    public class CalcChecker
+    public class PasswordChecker
     {
-        public static int summ(int nums, int num2)
+        static void Main(string[] args)
         {
-            
-            return nums+num2+1;
-            
+           Console.WriteLine(validatePassword("qwertY123"));
 
-        }
-        public static bool summ2(int nums, int num2)
+        } 
+        public static bool validatePassword(string password)
         {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
 
-            return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+
+            if (!password.Any(Char.IsUpper))
+                return false;
+
+            if (!password.Any(Char.IsDigit))
+                return false;
+
+            if (password.Intersect("!@#$%^&*()").Count() == 0)
+                return false;
 
 
+            return true;
         }
-        public static int razn(int nums3, int num4)
-        {
 
-            return nums3 - num4;
-
-
-        }
-        public static int proizv(int num11, int num22)
-        {
-
-            return num11 * num22;
-
-
-        }
-        public static float delenie(float num12, float num23)
-        {
-            string result = "";
-            try
-            {
-                return num12 / num23;
-            }
-            catch(DivideByZeroException)
-            {
-                result = "Divide by zero";
-            }
-            
-                return num12 / num23;
-            
-                
-            
-            
-
-
-        }
-        public static double sqrt(double a)
-        {
-
-            return Math.Sqrt(a);
-
-
-        }
-        public static double square(double a)
-        {
-
-            return Math.Pow(a, 2.0);
-
-
-        }
-        public static double factorial(double a)
-        {
-            double f = 1;
-
-            for (int i = 1; i <= a; i++)
-                f *= (double)i;
-            return f;
-        }
 
     }
+
 }
